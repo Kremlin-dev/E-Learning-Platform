d6bcd5330e997b2750735d9475715f08db0f3d58
AI-Amponsah <Yawamp27@gmail.com>
2023-07-21 01:56:50 +0000

    signup rendering

--- E_Learn/app.py
+++ E_Learn/app.py
@@ -1,20 +1,22 @@
-from flask import FLASK, render_template, session, redirect
+from flask import Flask, render_template, request, redirect, url_for
 
-app = FLASK(__name__)
 
-app.route('/')
+app = Flask(__name__)
+app.static_folder = 'static'
+
+@app.route('/')
 def index():
     return render_template('index.html')
 
-app.route('/signup', methods=['GET', 'POST'])
+@app.route('/signup', methods=['GET', 'POST'])
 def signup():
     return render_template('signup.html')
 
-app.route('/login', methods=['GET', 'POST'])
+@app.route('/login', methods=['GET', 'POST'])
 def login():
     return render_template('login.html')
 
-app.route('/coursepage', methods=['GET', 'POST'])
+@app.route('/coursepage', methods=['GET', 'POST'])
 def coursepage():
     return render_template('coursepage.html')
 
