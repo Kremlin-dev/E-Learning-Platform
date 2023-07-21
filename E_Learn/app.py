fafce9418b6fc3d7a806d49fe6fa33c73a3cce3d
AI-Amponsah <Yawamp27@gmail.com>
2023-07-21 02:49:58 +0000

    signup complete

--- E_Learn/app.py
+++ E_Learn/app.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template, request, redirect, url_for
+import psycopg2
 
-
+connection = psycopg2.connect(database="E-LEARNING", user="postgres", password="krem", host="localhost") 
+            
 app = Flask(__name__)
 app.static_folder = 'static'
 
@@ -10,10 +12,28 @@ def index():
 
 @app.route('/signup', methods=['GET', 'POST'])
 def signup():
+    if request.method=='POST':
+        first_name=request.form.get('firstname')
+        last_name=request.form.get('lastname')
+        email=request.form.get('email')
+        password=request.form.get('password')
+       
+        cursor = connection.cursor()
+        query= "INSERT INTO users(first_name, last_name, email, password)VALUES(%s, %s, %s, %s)"
+        try:
+            cursor.execute(query,(first_name, last_name, email, password))
+            connection.commit()
+            cursor.close()
+            return redirect('login')
+        except  psycopg2.errors.UniqueViolation:
+            error_message = "Email already exists. Please use a different email address."
+            return render_template('signup.html', error_message=error_message)
+
     return render_template('signup.html')
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    
     return render_template('login.html')
 
 @app.route('/coursepage', methods=['GET', 'POST'])
